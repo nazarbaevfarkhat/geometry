e6450a6f70c8d07fefacc011dc88f98c991639df
F_Nazarbaev <nazarbaevfarkhat@gmail.com>
2022-05-06 22:08:42 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+ "use strict";
+ 
+/* 
+let number = 5;
+const leftBorderWidth = 1;
+console.log(-4/0); // -Infinity
+console.log("Fara" * 9); // NaN
+const person = 'Farkhat';
+const bool = false;
+
+console.log(something);  // Null
+let und;
+console.log(und);  // undefined
+
+let obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(obj.name);    // 1 способ обращения к структурам
+console.log(obj["name"]); // 2 способ обращения к структурам
+
+
+alert("Привет");                     // вывод сообщения
+ let a = confirm("Вам уже есть 18?"); // вывод сообщения возврат true and false
+ let b = prompt("Как вас зовут","");  // вывод сообщения возврат string
+
+
+ // interpolation
+let user = "Azamat";
+alert(`Привет, ${user}!`);  // только с косыми чертами
+*/
+
+
+//Operations
+// ++, --, +, -, /, *, %
+// &&, ||, !  - and, or, not
+// 
+console.log("Hello"+250);  // string
+console.log(300 + +"34");  // unar + integer (number)
+console.log(5/2);
+
